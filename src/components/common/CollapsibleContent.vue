358972afa59bd589cdc37b92c7a00d16a669d977
Kai Blin <kblin@biosustain.dtu.dk>
2023-09-19 10:11:53 +0200

    collapsiblecontent: Make a bit prettier
    Signed-off-by: Kai Blin <kblin@biosustain.dtu.dk>

--- src/components/common/CollapsibleContent.vue
+++ src/components/common/CollapsibleContent.vue
@@ -6,8 +6,9 @@ import IconChevronRight from "../icons/IconChevronRight.vue";
 const props = withDefaults(
     defineProps<{
         collapsed?: boolean;
+        title?: string;
     }>(),
-    { collapsed: true }
+    { collapsed: true, title: "Click to expand" }
 );
 
 const collapsed = ref(props.collapsed);
@@ -21,15 +22,47 @@ const current_icon = computed(() => {
 </script>
 
 <template>
-    <div class="content">
+    <div class="header" :class="{ active: !collapsed }">
         <component :is="current_icon" @click="collapsed = !collapsed" />
-        <div v-if="collapsed" @click="collapsed = !collapsed">Click to expand</div>
-        <slot v-if="!collapsed"></slot>
+        <div class="collapsible-title" @click="collapsed = !collapsed">{{ props.title }}</div>
+    </div>
+    <div class="content" v-if="!collapsed">
+        <slot></slot>
     </div>
 </template>
 
 <style scoped>
+.header {
+    display: flex;
+    align-items: center;
+    touch-action: manipulation;
+    cursor: pointer;
+    border: 1px solid var(--color-border);
+    border-radius: 4px;
+    color: var(--color-text);
+    background-image: linear-gradient(
+        to bottom,
+        var(--color-background-soft) 0,
+        var(--color-background-mute) 100%
+    );
+    background-repeat: repeat-x;
+    margin-top: 0.25rem;
+    box-shadow: 0 1px 2px rgba(0, 0, 0, 0.05);
+    padding: 0.25rem 0.5rem;
+}
+
+.header.active {
+    border-bottom: 0;
+    border-bottom-left-radius: 0;
+    border-bottom-right-radius: 0;
+}
+
 .content {
     display: flex;
+    margin-bottom: 1rem;
+    border: 1px solid var(--color-border);
+    border-bottom-left-radius: 4px;
+    padding: 0.25rem;
+    border-bottom-right-radius: 4px;
 }
 </style>
